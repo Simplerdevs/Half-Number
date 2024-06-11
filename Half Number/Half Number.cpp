7128dd67e6a437bfc07b3ee745f3e69bddff1856
Simplerdevs <tplg7839@gmail.com>
2024-06-11 09:19:00 -0700

    Add project files.

---
+++ Half Number/Half Number.cpp
@@ -0,0 +1,26 @@
+								
+							/* Program to Print the half of Any number entered from user */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+
+{
+
+	float num;
+	cout << "Please Enter Any number in your mind \n";
+	cin >> num;
+
+
+	float result = num / 2;
+	
+	cout << " The Half of " << num << " = " << result << endl;
+
+
+	return 0;
+
+	// Created by @ilyes_Trabelsi
+
+}
